3c466689409eef09d975a1f945304be61e4a4821
mikalay <kolyatepl@gmail.com>
2025-06-04 10:43:52 +0200

    hihg php hardened and escaped

--- vulnerabilities/exec/source/high.php
+++ vulnerabilities/exec/source/high.php
@@ -1,37 +1,27 @@
 <?php
 
-if( isset( $_POST[ 'Submit' ]  ) ) {
-	// Get input
-	$target = trim($_REQUEST[ 'ip' ]);
+if (isset($_POST['Submit'])) {
+    // Get and trim the user input
+    $target = trim($_REQUEST['ip']);
 
-	// Set blacklist
-	$substitutions = array(
-		'||' => '',
-		'&'  => '',
-		';'  => '',
-		'| ' => '',
-		'-'  => '',
-		'$'  => '',
-		'('  => '',
-		')'  => '',
-		'`'  => '',
-	);
+    // Validate the input as a proper IP address
+    if (filter_var($target, FILTER_VALIDATE_IP)) {
+        // Safely escape the input for use in shell
+        $escaped_target = escapeshellarg($target);
 
-	// Remove any of the characters in the array (blacklist).
-	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+        // Detect OS and build command
+        if (stristr(php_uname('s'), 'Windows NT')) {
+            $cmd = shell_exec("ping $escaped_target");
+        } else {
+            $cmd = shell_exec("ping -c 4 $escaped_target");
+        }
 
-	// Determine OS and execute the ping command.
-	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
-		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
-	}
-	else {
-		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
-	}
+        // Output result
+        $html = "<pre>{$cmd}</pre>";
+    } else {
+        $html = "<pre>Invalid IP address provided.</pre>";
+    }
 
-	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
+    echo $html;
 }
-
 ?>
