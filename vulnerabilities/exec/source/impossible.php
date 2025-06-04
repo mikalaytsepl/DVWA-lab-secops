3a8afafbcbd53bf053f9d12cf55bef53a1e97414
mikalay <kolyatepl@gmail.com>
2025-06-04 11:04:13 +0200

    hardened for tainted-command-injection, mitigating fp

--- vulnerabilities/exec/source/impossible.php
+++ vulnerabilities/exec/source/impossible.php
@@ -1,38 +1,31 @@
 <?php
 
-if( isset( $_POST[ 'Submit' ]  ) ) {
-	// Check Anti-CSRF token
-	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
+if (isset($_POST['Submit'])) {
+    // Check Anti-CSRF token
+    checkToken($_REQUEST['user_token'], $_SESSION['session_token'], 'index.php');
 
-	// Get input
-	$target = $_REQUEST[ 'ip' ];
-	$target = stripslashes( $target );
+    // Get and trim input
+    $target = trim($_REQUEST['ip']);
 
-	// Split the IP into 4 octects
-	$octet = explode( ".", $target );
+    // Validate the input as a proper IP address
+    if (filter_var($target, FILTER_VALIDATE_IP)) {
+        // Determine OS and execute the ping command securely
+        if (stristr(php_uname('s'), 'Windows NT')) {
+            // Windows
+            $cmd = shell_exec('ping ' . escapeshellarg($target));
+        } else {
+            // *nix
+            $cmd = shell_exec('ping -c 4 ' . escapeshellarg($target));
+        }
 
-	// Check IF each octet is an integer
-	if( ( is_numeric( $octet[0] ) ) && ( is_numeric( $octet[1] ) ) && ( is_numeric( $octet[2] ) ) && ( is_numeric( $octet[3] ) ) && ( sizeof( $octet ) == 4 ) ) {
-		// If all 4 octets are int's put the IP back together.
-		$target = $octet[0] . '.' . $octet[1] . '.' . $octet[2] . '.' . $octet[3];
+        // Feedback for the end user
+        $html = "<pre>{$cmd}</pre>";
+    } else {
+        // Notify the user of the error
+        $html = '<pre>ERROR: You have entered an invalid IP.</pre>';
+    }
 
-		// Determine OS and execute the ping command.
-		if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
-			// Windows
-			$cmd = shell_exec( 'ping  ' . $target );
-		}
-		else {
-			// *nix
-			$cmd = shell_exec( 'ping  -c 4 ' . $target );
-		}
-
-		// Feedback for the end user
-		$html .= "<pre>{$cmd}</pre>";
-	}
-	else {
-		// Ops. Let the user name theres a mistake
-		$html .= '<pre>ERROR: You have entered an invalid IP.</pre>';
-	}
+    echo $html;
 }
 
 // Generate Anti-CSRF token
